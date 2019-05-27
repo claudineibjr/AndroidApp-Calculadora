22bd838ab1f01ea3d71555b432a3a9b7c4df2df7
ClaudineiBJr <claudineibjr@hotmail.com>
2019-05-27 10:58:51 -0300

    Adjust of layouts

--- app/src/main/java/com/claudineiBJr/calculadora/MainActivity.java
+++ app/src/main/java/com/claudineiBJr/calculadora/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_constraint);
+        setContentView(R.layout.activity_main_constraintlayout);
 
         setViewElements();
     }
